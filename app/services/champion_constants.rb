050a508dc359641466771ac00ec07d7dd3a35532
Zachary Allaun <zach.allaun@gmail.com>
2016-05-01 14:08:35 -0400

    Example ChampionConstants

---
+++ app/services/champion_constants.rb
@@ -0,0 +1,11 @@
+module ChampionConstants
+  RELEASE_DATES = {
+    "Aatrox" => Date.parse("July 3 2015")
+  }
+
+  NICKNAMES = {
+    "Teemo" => "the Literal Devil"
+  }
+
+  # Access elsewhere with ChampionConstants::RELEASE_DATES[champion.name]
+end
