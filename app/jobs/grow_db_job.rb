9c9908f5ca7bb2f505e35c6ad834cb64b2db40c7
Zachary Allaun <zach.allaun@gmail.com>
2016-05-03 00:24:22 -0400

    GrowDbJob

---
+++ app/jobs/grow_db_job.rb
@@ -0,0 +1,13 @@
+class GrowDbJob < ActiveJob::Base
+  def perform(final_target_size)
+    n = Summoner.count
+
+    if n >= final_target_size
+      FetchMastery.new.update_without_fetch
+    else
+      GenIds.new.grow_until(n + 5)
+      FetchMastery.new.fetch_outdated
+      GrowDbJob.perform_later(final_target_size)
+    end
+  end
+end
