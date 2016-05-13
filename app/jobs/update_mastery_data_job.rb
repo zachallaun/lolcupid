2072474d3a9712f2c46d33f8f3a5d4e9398bd0f5
Zachary Allaun <zach.allaun@gmail.com>
2016-05-13 12:05:30 -0400

    Decrease resources needed for UpdateMasteryDataJob

--- app/jobs/update_mastery_data_job.rb
+++ app/jobs/update_mastery_data_job.rb
@@ -1,6 +1,6 @@
 class UpdateMasteryDataJob < ActiveJob::Base
   def perform
-    FetchMastery.new.update_outdated_mastery_data(region: "na", limit: 1000, threads: 3)
-    FetchMastery.new.update_outdated_mastery_data(region: "euw", limit: 1000, threads: 3)
+    FetchMastery.new.update_outdated_mastery_data(region: "na", limit: 500, threads: 2)
+    FetchMastery.new.update_outdated_mastery_data(region: "euw", limit: 500, threads: 2)
   end
 end
